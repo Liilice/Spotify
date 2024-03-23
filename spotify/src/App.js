75fac074c55af51dbe8a6fdcf241017e6508ca79
Henry <henry.alexandre-dos-santos@epitech.eu>
2024-03-23 11:08:09 +0100

    Router, Routes, Route, Link, Navigate, Params

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,25 +1,39 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NewFile from "./test.jsx";
 import logo from './logo.svg';
 import './App.css';
 
+function MainPage() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Link to={`/caca`}>Go to New File</Link>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/caca" element={<NewFile />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ spotify/src/components/largeDiv.jsx
@@ -0,0 +1,11 @@
+// import { useParams, useNavigate } from 'react-router-dom';
+
+function LargeDiv(background, textColor) {
+    return (
+        <div style={{width: 50, height: 50, color: textColor}}>
+            <h1>Large div</h1>
+        </div>
+    );
+}
+
+export default LargeDiv;
