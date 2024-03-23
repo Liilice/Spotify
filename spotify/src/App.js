7325570dd03ed0990c63e0b5734342dfc23e35bd
Liilice <alice.zheng@epitech.eu>
2024-03-23 15:25:15 +0100

    details_album_sans_design

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+// import { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+// function App() {
+//   const [artiste, setArtiste] = useState([]);
+//   URLSearchParams
+//   const url = "http://localhost:8000/artists/"+;
+//   function fetch_artiste() {
+//     let response;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Details_album from "./Details_album";
+import logo from "./logo.svg";
+import "./App.css";
+
+function MainPage() {
+  const album = 1;
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +47,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Link to={`/details_album/${album}`}>Go to New File</Link>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +58,17 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/details_album/:album" element={<Details_album />} />
+      </Routes>
+    </Router>
   );
 }
 
