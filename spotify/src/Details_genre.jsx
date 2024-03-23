b9a6e7b5da43ed84fba38a055de2f333da0a1fcd
Liilice <alice.zheng@epitech.eu>
2024-03-23 22:14:08 +0100

    Detail_artiste_done

--- spotify/src/Details_genre.jsx
+++ spotify/src/Details_genre.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
+import BtnBack from "./components/btnBack";
+import AlbumsGenre from "./components/AlbumsGenre";
+import Pagination from "./components/pagination";
 
 function Details_genre() {
   const [albums, setAlbums] = useState([]);
   const [all_albums, setAll_albums] = useState([]);
   const [artistes, setArtistes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(6);
+
   let album_id = useParams();
   useEffect(() => {
     const fetchData = async () => {
@@ -14,17 +19,13 @@ function Details_genre() {
         "http://localhost:8000/genres/" + album_id.genre
       );
       const data = await response.json();
-      //   console.log(data);
       setAlbums(data.albums);
-      //   console.log(albums);
       const all_albums = await fetch("http://localhost:8000/albums");
       const data_albums = await all_albums.json();
-      //   console.log(data_albums);
       setAll_albums(data_albums);
 
       const artistes = await fetch("http://localhost:8000/artists");
       const data_artistes = await artistes.json();
-      //   console.log(data_albums);
       setArtistes(data_artistes);
     };
     fetchData();
@@ -44,7 +45,7 @@ function Details_genre() {
 
   function check() {
     let result = album_artiste();
-    console.log(result);
+    // console.log(result);
     let arr = [];
     result.map((album) => {
       let album_id = album[0].id;
@@ -57,18 +58,41 @@ function Details_genre() {
     return arr;
   }
   let albu = check();
+  //   console.log(albu);
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = albu.splice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(albu.length / recordsPerPage);
 
   return (
-    <div className="App">
-      {console.log(albu)}
-      {albu.map((album) => (
-        <ul>
-          <li>nom : {album[0].name}</li>
-          <li>auteur : {album[1]}</li>
-        </ul>
-      ))}
-
-      <p>test</p>
+    <div
+      style={{ backgroundColor: "#282828", width: "100%", minHeight: "100vh" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "5vh",
+        }}
+      >
+        <BtnBack url="/" />
+        <h1 style={{ fontSize: "75px", marginRight: "5vh" }}>Details Genre</h1>
+      </div>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {/* {console.log(albu)} */}
+        {albu.map((album) => (
+          <AlbumsGenre info={album} />
+        ))}
+      </div>
+      {/* <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      /> */}
     </div>
   );
 }
