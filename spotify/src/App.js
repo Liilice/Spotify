cecd72bca7e3962ce394be88a0475ab76d90091e
Henry <henry.alexandre-dos-santos@epitech.eu>
2024-03-24 01:07:57 +0100

    Fix bugs

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import LargeDiv2Text from "./components/largeDiv.jsx";
 import Details_album from "./Details_album.jsx";
+import Details_artiste from "./Details_artiste.jsx";
+// import Details_genre from "./Details_genre.jsx";
 import ListingAlbums from "./listing_album.js";
 import ListingGenre from "./listing_genre.js";
 import ListingArtists from "./listing_artist.js";
@@ -68,7 +70,9 @@ function App() {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/search" element={<SearchPage />} />
                 <Route path="/listing_album" element={<ListingAlbums />} />
-                {/*<Route path="/details_album/:album" element={<Details_album />} />*/}
+                <Route path="/Details_album/:album" element={<Details_album />} />
+                <Route path="/details_artiste/:artiste" element={<Details_artiste />} />
+                {/*<Route path="/details_genre/:genre" element={<Details_genre />} />*/}
                 <Route path="/listing_genre" element={<ListingGenre />} />
                 <Route path="/listing_artists" element={<ListingArtists />} />
             </Routes>
